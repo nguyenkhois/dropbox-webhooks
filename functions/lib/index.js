4718b22fc88a475878302bb17012c5431b76b520
Khoi Le <minarek@outlook.com>
2018-05-30 14:34:41 +0200

    Created Dropbox Webhooks module

---
+++ functions/lib/index.js
@@ -0,0 +1,62 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const firebaseAdmin = require("firebase-admin");
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    const responseHeaders = {
+        'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type'
+    };
+    response.status(200)
+        .set(responseHeaders)
+        .send("Hello from Firebase with Cloud Functions! :-)");
+});
+exports.webhooks = functions.https.onRequest((request, response) => {
+    const defaultHeaders = {
+        'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST',
+        'Access-Control-Allow-Headers': 'Content-Type'
+    };
+    switch (request.method) {
+        case 'GET':
+            const queryString = request.query.challenge;
+            const responseHeaders = {
+                'Content-Type': 'text/plain',
+                'X-Content-Type-Options': 'nosniff'
+            };
+            response.status(200)
+                .set(responseHeaders)
+                .send(queryString);
+            break;
+        case 'POST':
+            firebaseAdmin.initializeApp(functions.config().firebase);
+            const db = firebaseAdmin.firestore();
+            db.collection('dbxWebhooks').doc('receiveData').set({
+                data: request.body,
+                writeTime: Date.now()
+            })
+                .then(function (docRef) {
+                response.status(200)
+                    .set(defaultHeaders)
+                    .send('Process successfully');
+            })
+                .catch(function (error) {
+                response.status(400)
+                    .set(defaultHeaders)
+                    .send(error);
+            });
+            break;
+        default:
+            response.status(400)
+                .set(defaultHeaders)
+                .send('Bad Request');
+            break;
+    }
+});
+//# sourceMappingURL=index.js.map
